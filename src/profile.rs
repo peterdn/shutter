e5bcb5025f4ff883e86be3c8ec1dd117c4e6fd82
Peter Nelson <peter@peterdn.com>
2018-06-09 15:08:00 -0500

    Refactor and apply rustfmt
    * Made username and is_private fields no longer optional
    * Format code with rustfmt

--- src/profile.rs
+++ src/profile.rs
@@ -3,21 +3,26 @@ use scrape;
 
 #[derive(Debug, PartialEq)]
 pub struct Profile {
-    pub username: Option<String>,
+    pub username: String,
     pub full_name: Option<String>,
     pub biography: Option<String>,
     pub external_url: Option<String>,
     pub profile_pic: Option<Image>,
-    pub is_private: Option<bool>,
+    pub is_private: bool,
     pub images: Vec<Image>,
 }
 
 impl From<scrape::JsonProfile> for Profile {
     fn from(json_profile: scrape::JsonProfile) -> Profile {
         let profile_pic_image = json_profile.profile_pic_url_hd.map(|url| Image { url });
-        let images = json_profile.edge_owner_to_timeline_media.edges.iter().map(|ref edge| {
-            Image { url: edge.node.display_url.clone() }
-        }).collect::<Vec<Image>>();
+        let images = json_profile
+            .edge_owner_to_timeline_media
+            .edges
+            .iter()
+            .map(|ref edge| Image {
+                url: edge.node.display_url.clone(),
+            })
+            .collect::<Vec<Image>>();
 
         Profile {
             username: json_profile.username,
@@ -26,7 +31,7 @@ impl From<scrape::JsonProfile> for Profile {
             external_url: json_profile.external_url,
             profile_pic: profile_pic_image,
             is_private: json_profile.is_private,
-            images
+            images,
         }
     }
 }
@@ -47,31 +52,47 @@ mod tests {
     #[test]
     fn test_from_json_profile() {
         let json_profile = JsonProfile {
-            username: Some("peterdn".to_string()),
+            username: "peterdn".to_string(),
             full_name: Some("Peter Nelson".to_string()),
             biography: Some("test biography".to_string()),
             external_url: Some("https://peterdn.com".to_string()),
             profile_pic_url_hd: Some("https://peterdn.com/profile.jpg".to_string()),
-            is_private: Some(true),
+            is_private: true,
             edge_owner_to_timeline_media: JsonEdgeOwnerToTimelineMedia {
-                edges: vec![JsonEdge {
-                    node: JsonNode {display_url: "https://peterdn.com/1.jpg".to_string()}
-                }, JsonEdge {
-                    node: JsonNode {display_url: "https://peterdn.com/2.jpg".to_string()}
-                }]
-            }
+                edges: vec![
+                    JsonEdge {
+                        node: JsonNode {
+                            display_url: "https://peterdn.com/1.jpg".to_string(),
+                        },
+                    },
+                    JsonEdge {
+                        node: JsonNode {
+                            display_url: "https://peterdn.com/2.jpg".to_string(),
+                        },
+                    },
+                ],
+            },
         };
-        assert_eq!(Profile::from(json_profile), Profile {
-            username: Some("peterdn".to_string()),
-            full_name: Some("Peter Nelson".to_string()),
-            biography: Some("test biography".to_string()),
-            external_url: Some("https://peterdn.com".to_string()),
-            profile_pic: Some(Image { url: "https://peterdn.com/profile.jpg".to_string() }),
-            is_private: Some(true),
-            images: vec![
-                Image {url: "https://peterdn.com/1.jpg".to_string()},
-                Image {url: "https://peterdn.com/2.jpg".to_string()},
-            ]
-        });
+        assert_eq!(
+            Profile::from(json_profile),
+            Profile {
+                username: "peterdn".to_string(),
+                full_name: Some("Peter Nelson".to_string()),
+                biography: Some("test biography".to_string()),
+                external_url: Some("https://peterdn.com".to_string()),
+                profile_pic: Some(Image {
+                    url: "https://peterdn.com/profile.jpg".to_string()
+                }),
+                is_private: true,
+                images: vec![
+                    Image {
+                        url: "https://peterdn.com/1.jpg".to_string(),
+                    },
+                    Image {
+                        url: "https://peterdn.com/2.jpg".to_string(),
+                    },
+                ],
+            }
+        );
     }
 }
