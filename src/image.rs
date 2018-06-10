05860897062715b4758474dcc121788f0e7719f8
Peter Nelson <peter@peterdn.com>
2018-06-09 20:39:33 -0500

    Extract timestamp uploaded for each image

--- src/image.rs
+++ src/image.rs
@@ -1,4 +1,21 @@
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
 #[derive(Clone, Debug, PartialEq)]
-pub struct Image {
+pub struct PostImage {
+    pub url: String,
+    pub timestamp_uploaded: SystemTime,
+}
+
+impl PostImage {
+    pub fn new(url: &str, timestamp: i32) -> PostImage {
+        PostImage {
+            url: url.to_string(),
+            timestamp_uploaded: UNIX_EPOCH + Duration::from_secs(timestamp as u64),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ProfileImage {
     pub url: String,
 }
