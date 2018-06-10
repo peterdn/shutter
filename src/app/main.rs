4080ecfd5f1067f55e19f96226b12b6659a9ce88
Peter Nelson <peter@peterdn.com>
2018-06-09 21:03:13 -0500

    Save images with their timestamp as filename

--- src/app/main.rs
+++ src/app/main.rs
@@ -8,8 +8,11 @@ use clap::App;
 use rayon::prelude::*;
 use std::fs::File;
 use std::io::BufWriter;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process;
+use std::time::UNIX_EPOCH;
+
+use shutter::image::PostImage;
 
 const EXIT_SUCCESS: i32 = 0;
 const EXIT_FAILURE: i32 = 1;
@@ -38,6 +41,16 @@ fn print_profile(user_profile: &shutter::profile::Profile) {
     );
 }
 
+fn get_local_filepath_for_image(outdir: &Path, img: &PostImage) -> PathBuf {
+    let timestamp_s = img
+        .timestamp_uploaded
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let filename = format!("{}.jpg", timestamp_s);
+    outdir.join(filename)
+}
+
 fn download_images(user_profile: &shutter::profile::Profile, outdir: &Path) {
     if outdir.exists() && !outdir.is_dir() {
         panic!("A file exists with the same name as the output directory!");
@@ -47,10 +60,9 @@ fn download_images(user_profile: &shutter::profile::Profile, outdir: &Path) {
 
     println!("Downloading {} images...", user_profile.images.len());
     user_profile.images.par_iter().for_each(|ref img| {
-        let filename = img.url.rsplit('/').collect::<Vec<&str>>()[0];
-        let filename = outdir.join(filename);
-        println!("Downloading {}", filename.to_string_lossy());
-        let image_file = File::create(filename).unwrap();
+        let filepath = get_local_filepath_for_image(&outdir, &img);
+        println!("Downloading {}", filepath.to_string_lossy());
+        let image_file = File::create(filepath).unwrap();
         let mut writer = BufWriter::new(image_file);
         reqwest::get(&img.url)
             .unwrap()
@@ -80,5 +92,25 @@ fn main() {
     } else {
         println!("User {} could not be found", username);
         process::exit(EXIT_FAILURE);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::time::{Duration, UNIX_EPOCH};
+    use super::*;
+
+    #[test]
+    fn test_get_local_filepath_for_image() {
+        let timestamp = UNIX_EPOCH + Duration::from_secs(1230000000);
+        let image = PostImage {
+            url: "doesntmatter.jpg".to_string(),
+            timestamp_uploaded: timestamp,
+        };
+        let outdir = Path::new("/test/dir");
+        assert_eq!(
+            get_local_filepath_for_image(&outdir, &image),
+            Path::new("/test/dir/1230000000.jpg")
+        );
     }
 }
